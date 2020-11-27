aa26d5e9f38e9efb2d79a2276eba8ec9dbbcfc09
Jakub Kolodziejczyk <>
2020-11-27 21:41:59 +0900

    Added net.ml module
    Contains segmentation model builder

--- docker/app.Dockerfile
+++ docker/app.Dockerfile
@@ -3,7 +3,12 @@
 FROM tensorflow/tensorflow:2.3.1-gpu
 
 # Install a few necessary need or useful libs and apps
-RUN apt update && apt install -y wget vim libgl1-mesa-dev
+RUN apt update && apt install -y wget vim libgl1-mesa-dev ack
+
+# Download base tensorflow model to app_user's folder, change permission so he can use it
+RUN mkdir -p /root/.keras/models && \
+    wget https://storage.googleapis.com/tensorflow/keras-applications/resnet/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5 \
+        -O /root/.keras/models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5
 
 # Install python environment
 COPY ./requirements.txt /tmp/requirements.txt
--- net/invoke/ml.py
+++ net/invoke/ml.py
@@ -6,89 +6,100 @@ import invoke
 
 
 @invoke.task
-def train(_context, config_path):
+def train(_context, config_path, load_existing_model=False):
     """
     Task to train drivable areas segmentation model
 
     Args:
         _context (invoke.Context): context instance
         config_path (str): path to configuration file
+        load_existing_model (bool): specifies if existing model should be loaded, instead of training from scratch.
+        Defaults to False
     """
 
-    import icecream
+    import mlflow
     import tensorflow as tf
 
     import net.data
+    import net.ml
     import net.utilities
 
     config = net.utilities.read_yaml(config_path)
 
-    training_samples_loader = net.data.BDDSamplesDataLoader(
-        images_directory=config["training_images_directory"],
-        segmentations_directory=config["training_segmentations_directory"],
-        labels_path=config["training_labels_directory"]
-    )
+    mlflow.set_tracking_uri(config["mlflow_tracking_uri"])
+    mlflow.set_experiment("training")
 
-    training_data_loader = net.data.TrainingDataLoader(
-        samples_data_loader=training_samples_loader,
-        batch_size=config["batch_size"],
-        use_training_mode=True
-    )
+    with mlflow.start_run(run_name="simple_run"):
 
-    training_dataset = tf.data.Dataset.from_generator(
-        generator=lambda: iter(training_data_loader),
-        output_types=(tf.float32, tf.float32),
-        output_shapes=(
-            tf.TensorShape([
-                None,
-                config["training_image_dimensions"]["height"],
-                config["training_image_dimensions"]["width"],
-                3
-            ]),
-            tf.TensorShape([
-                None,
-                config["training_image_dimensions"]["height"],
-                config["training_image_dimensions"]["width"]
-            ])
+        mlflow.tensorflow.autolog(every_n_iter=1)
+
+        training_samples_loader = net.data.BDDSamplesDataLoader(
+            images_directory=config["training_images_directory"],
+            segmentations_directory=config["training_segmentations_directory"],
+            labels_path=config["training_labels_directory"]
         )
-    ).prefetch(32)
 
-    training_data = list(training_dataset.take(2))
-
-    icecream.ic(training_data[0][0].shape)
-    icecream.ic(training_data[0][1].shape)
-
-    validatation_samples_loader = net.data.BDDSamplesDataLoader(
-        images_directory=config["validation_images_directory"],
-        segmentations_directory=config["validation_segmentations_directory"],
-        labels_path=config["validation_labels_directory"]
-    )
-
-    validation_data_loader = net.data.TrainingDataLoader(
-        samples_data_loader=validatation_samples_loader,
-        batch_size=config["batch_size"],
-        use_training_mode=False
-    )
-
-    validation_dataset = tf.data.Dataset.from_generator(
-        generator=lambda: iter(validation_data_loader),
-        output_types=(tf.float32, tf.float32),
-        output_shapes=(
-            tf.TensorShape([
-                None,
-                config["training_image_dimensions"]["height"],
-                config["training_image_dimensions"]["width"],
-                3
-            ]),
-            tf.TensorShape([
-                None,
-                config["training_image_dimensions"]["height"],
-                config["training_image_dimensions"]["width"]
-            ])
+        training_data_loader = net.data.TrainingDataLoader(
+            samples_data_loader=training_samples_loader,
+            batch_size=config["batch_size"],
+            use_training_mode=True
         )
-    ).prefetch(32)
 
-    validation_data = list(validation_dataset.take(2))
+        training_dataset = tf.data.Dataset.from_generator(
+            generator=lambda: iter(training_data_loader),
+            output_types=(tf.float32, tf.float32),
+            output_shapes=(
+                tf.TensorShape([
+                    None,
+                    config["training_image_dimensions"]["height"],
+                    config["training_image_dimensions"]["width"],
+                    3
+                ]),
+                tf.TensorShape([
+                    None,
+                    config["training_image_dimensions"]["height"],
+                    config["training_image_dimensions"]["width"]
+                ])
+            )
+        ).prefetch(32)
 
-    icecream.ic(validation_data[0][0].shape)
-    icecream.ic(validation_data[0][1].shape)
+        validatation_samples_loader = net.data.BDDSamplesDataLoader(
+            images_directory=config["validation_images_directory"],
+            segmentations_directory=config["validation_segmentations_directory"],
+            labels_path=config["validation_labels_directory"]
+        )
+
+        validation_data_loader = net.data.TrainingDataLoader(
+            samples_data_loader=validatation_samples_loader,
+            batch_size=config["batch_size"],
+            use_training_mode=False
+        )
+
+        validation_dataset = tf.data.Dataset.from_generator(
+            generator=lambda: iter(validation_data_loader),
+            output_types=(tf.float32, tf.float32),
+            output_shapes=(
+                tf.TensorShape([
+                    None,
+                    config["training_image_dimensions"]["height"],
+                    config["training_image_dimensions"]["width"],
+                    3
+                ]),
+                tf.TensorShape([
+                    None,
+                    config["training_image_dimensions"]["height"],
+                    config["training_image_dimensions"]["width"]
+                ])
+            )
+        ).prefetch(32)
+
+        model = tf.keras.models.load_model(
+            filepath=config["current_model_directory"],
+            custom_objects={
+                "get_temperature_scaled_sparse_softmax": net.ml.get_temperature_scaled_sparse_softmax}
+        ) if load_existing_model else \
+            net.ml.DeepLabV3PlusBuilder().get_model(categories_count=len(config["categories"]))
+
+        print(training_dataset)
+        print(validation_dataset)
+        print(model)
